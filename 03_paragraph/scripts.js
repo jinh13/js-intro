ac353a3f819b21b50f4a868448e61c2d08df4349
Heather Jin <Heather@heathers-air.dhcp.wustl.edu>
2016-04-11 18:43:43 -0500

    paragraph 3

---
+++ 03_paragraph/scripts.js
@@ -0,0 +1,35 @@
+document.write("<h1>Example Title</h1>");
+document.write("<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+	Suspendisse sollicitudin nibh in est faucibus lobortis. 
+	Curabitur dignissim blandit turpis, ut aliquet neque. 
+	In iaculis vulputate turpis vitae fermentum. Aliquam nisi augue, 
+	egestas ut cursus sit amet, viverra eget odio. Donec sed sapien 
+	at nisl consequat ornare eu in ligula. Sed lobortis tempus nulla 
+	ut lacinia. Vestibulum tempor nec lorem nec feugiat. In vel nisi 
+	et leo pellentesque finibus. Phasellus augue justo, laoreet quis 
+	efficitur eu, dignissim nec odio.</p>");
+
+document.write("<p>Aliquam semper sem magna, id sagittis nunc 
+	condimentum in. Phasellus enim lorem, ullamcorper vel cursus eu, 
+	euismod vel est. Curabitur in ultrices sapien. Vivamus a 
+	fermentum ipsum. Cras scelerisque porttitor felis, at euismod 
+	est aliquet id. Pellentesque posuere luctus euismod. Vivamus 
+	massa odio, fermentum in lobortis ut, varius eu velit. Aenean 
+	ut ornare nulla, non dignissim tortor. Donec gravida vehicula 
+	imperdiet. Nam id augue sit amet est pharetra mollis. Proin 
+	eros dolor, venenatis eu ultrices sit amet, venenatis eget 
+	ante. Sed vitae nisi non ligula convallis viverra.</p>");
+
+document.write("<p>Aenean ac lobortis purus. Phasellus quis 
+	nulla at sem eleifend ornare et vel ante. Pellentesque 
+	fermentum imperdiet augue, ac faucibus ipsum ornare ut. 
+	Mauris bibendum finibus sapien, vel fermentum erat accumsan 
+	id. Maecenas id quam a ligula auctor scelerisque eu quis 
+	lorem. Sed facilisis, massa quis venenatis pellentesque, 
+	dui lacus sollicitudin metus, eu luctus dolor eros nec mauris. 
+	Sed fringilla risus mollis augue blandit sollicitudin. Ut rhoncus
+	 egestas ante nec tristique. Nullam blandit dolor tincidunt 
+	 nisi faucibus, a ultricies lacus tristique. Vestibulum sit 
+	 amet blandit nibh, a condimentum nunc.</p>");
+
+
